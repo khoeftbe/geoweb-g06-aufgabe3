db3f90544fb082186214d24a281146b1d199a736
annakarn <33088793+annakarn@users.noreply.github.com>
2017-11-03 16:22:26 +0100

    added index.html

---
+++ main.js
@@ -0,0 +1,46 @@
+import 'ol/ol.css';
+import Map from 'ol/map';
+import View from 'ol/view';
+import TileLayer from 'ol/layer/tile';
+import Stamen from 'ol/source/stamen';
+import VectorLayer from 'ol/layer/vector';
+import Vector from 'ol/source/vector';
+import GeoJSON from 'ol/format/geojson';
+import Style from 'ol/style/style';
+import Text from 'ol/style/text';
+import Stroke from 'ol/style/stroke';
+import proj from 'ol/proj';
+
+const map = new Map({
+  target: 'map',
+  view: new View({
+    center: proj.fromLonLat([16.37, 48.2]),
+    zoom: 13
+  })
+});
+map.addLayer(new TileLayer({
+  source: new Stamen({
+    layer: 'watercolor'
+  })
+}));
+
+const layer = new VectorLayer({
+source: new Vector({
+url: 'data/map.geojson',
+format: new GeoJSON()
+})
+});
+map.addLayer(layer);
+
+layer.setStyle(function(feature) {
+return new Style({
+text: new Text({
+text: feature.get('name'),
+font: 'Bold 8pt Verdana',
+stroke: new Stroke({
+color: 'white',
+width: 3
+})
+})
+});
+});
